18fde98a8210da8ef824694a1c00040b35841ef0
jacek <jpajak@yandex.com>
2018-05-06 12:40:54 +0200

    Adding supply

---
+++ server/fixtures.js
@@ -0,0 +1,11 @@
+
+import faker from 'faker'
+
+if (Statuses.find().count() === 0) {
+  for (let i =0; i<=100; i++) {
+    Statuses.insert({
+      message: faker.name.findName(),
+      userId: 'grpMnw6grnCxyywn3',
+    });
+  }
+}
